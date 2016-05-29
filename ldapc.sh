523dcaa5023aafce1580f2ffeb05ac9a1a9cf731
haw526 <haw526@162.com>
2016-05-29 21:23:52 +0800

    new

--- ldapc.sh
+++ ldapc.sh
@@ -21,26 +21,24 @@ authconfig \
 --enablecachecreds \
 --update
 
-#wget 下载证书
-wget -O $CADIR/ldap.ca http://$SERVER/CA 
 
 # 修改sssd配置文件
 cat > /etc/sssd/sssd.conf << EOF
 [domain/default]
 enumerate=True
-autofs_provider = ldap
 ldap_schema = rfc2307bis
 ldap_search_base = $BASEDN
 id_provider = ldap
 auth_provider = ldap
 chpass_provider = ldap
+ldap_user_ssh_public_key=sshPublicKey
 ldap_uri = ldaps://$SERVER:636
 ldap_id_use_start_tls = True
 cache_credentials = True
 ldap_tls_cacertdir = $CADIR
 ldap_tls_cacert = $CADIR/ldap.ca
 [sssd]
-services = nss, pam, autofs
+services = nss, pam, ssh, sudo
 config_file_version = 2
 domains = default
 [nss]
@@ -51,4 +49,6 @@ homedir_substring = /home
 [ssh]
 EOF
 
+#wget 下载证书
+wget -O $CADIR/ldap.ca http://$SERVER/CA 
 /etc/init.d/sssd restart
